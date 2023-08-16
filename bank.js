efcad685b300fd579ab167b405fba67ea0a63109
arafatshabbir2 <arafatshabbir8@gmail.com>
2023-08-16 12:59:13 +0600

    cheaked number or not

--- bank.js
+++ bank.js
@@ -1,19 +1,32 @@
-document.getElementById('deposit-btn').addEventListener('click',function(){
-    let deposit = document.getElementById('deposit-amount');
-    let newdepositAmount = document.getElementById('deposit-amount').value
-    let previousDepositamount = document.getElementById('total-deposit')
-    previousDepositamount.innerText = parseFloat(previousDepositamount.innerText) + parseFloat(newdepositAmount)
-    deposit.value = '';
-    let total = document.getElementById('total-amount')
-    total.innerText= parseFloat(total.innerText) + parseFloat(newdepositAmount)
+document.getElementById('deposit-btn').addEventListener('click', function () {
+  let deposit = document.getElementById('deposit-amount');
+  let newdepositAmount = document.getElementById('deposit-amount').value
+  let totalDepositamount = document.getElementById('total-deposit')
+  deposit.value = '';
+  if (isNaN(newdepositAmount)) {
+    alert('please provide a number')
+    return
+  }
+  totalDepositamount.innerText = parseFloat(totalDepositamount.innerText) + parseFloat(newdepositAmount)
+  let total = document.getElementById('total-amount')
+  total.innerText = parseFloat(total.innerText) + parseFloat(newdepositAmount)
 })
-document.getElementById('withdraw-btn').addEventListener('click',function(){
-  let withdraw= document.getElementById('withdraw-amount')
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+  let withdraw = document.getElementById('withdraw-amount')
   let withdrawamount = document.getElementById('withdraw-amount').value
   let totalwithdraw = document.getElementById('total-withdraw')
-  totalwithdraw.innerText = parseFloat(totalwithdraw.innerText) + parseFloat(withdrawamount)
   withdraw.value = '';
+  if (isNaN(withdrawamount)) {
+    alert('please provide a number')
+    return
+  }
   let total = document.getElementById('total-amount')
-  total.innerText= parseFloat(total.innerText) - parseFloat(withdrawamount)
+  if (withdrawamount > total.innerText) {
+    alert("you can't witdraw more money then your total balance")
+  } else {
+    total.innerText = parseFloat(total.innerText) - parseFloat(withdrawamount)
+    totalwithdraw.innerText = parseFloat(totalwithdraw.innerText) + parseFloat(withdrawamount)
+  }
+
 
 })
